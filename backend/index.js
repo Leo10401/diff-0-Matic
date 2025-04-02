d4e8da581ba4ec04bf3875c3e089d9312a48da81
Leo10401 <hunterleo626217@gmail.com>
2025-04-02 17:30:18 +0530

    'complete'

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://diff-0-matic.vercel.app/"],
     methods: "GET,POST",
     allowedHeaders: "Content-Type,Authorization"
 }));
